689d8d4934a945b21ba89117d4edf1a0c3587180
ChVidoix <rolester92@gmail.com>
2021-04-20 13:58:11 +0200

    style: basic styles and scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,51 @@
-import React, { useRef, useState, useCallback, useEffect, ForwardedRef } from 'react';
+import React, { useRef, useState, useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { Dispatch } from 'redux'
 import { rootStore } from './store/store'
 import { getCharacters, getFilms } from './actions/appActions'
 import { appState } from './reducers/appReducer'
 import { v4 as uuidv4 } from 'uuid';
-import './App.css';
 import Character from './components/Character';
-import { Table, TableCell, TableContainer, TableHead, TableBody, TableRow } from '@material-ui/core'
+import { Table, TableCell, TableContainer, TableHead, TableBody, TableRow, makeStyles, Paper, ThemeProvider, createMuiTheme } from '@material-ui/core'
+import MainWrapper from './styled/MainWrapper'
+
+const useStyles = makeStyles({
+  root: {
+    width: '90%',
+    textAlign: 'center',
+    borderRadius: 15,
+    height: '90vh'
+  },
+  container: {
+    height: '90%',
+    borderRadius: 15,
+    '&::-webkit-scrollbar': {
+      width: '0.4em',
+    },
+    '&::-webkit-scrollbar-track': {
+      boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+      webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)'
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: 'rgba(0,0,0,.1)',
+      borderRadius: '3px'
+    },
+    '&::-webkit-scrollbar-track-piece:start': {
+      background: 'transparent',
+      marginTop: '10px'
+    }
+  }
+})
 
 const App: React.FC = () => {
+
+  const theme = createMuiTheme({
+    palette: {
+      type: 'dark'
+    }
+  })
+
+  const classes = useStyles()
 
   const dispatch: Dispatch<any> = useDispatch()
   const observer: React.MutableRefObject<any> = useRef()
@@ -36,43 +72,47 @@ const App: React.FC = () => {
   }, [])
 
   return (
+    <ThemeProvider theme={theme}>
+      <MainWrapper>
+        <Paper className={classes.root}>
+          {characters && (
 
-    <div className="App">
-      {characters && (
-        <div>
-          <TableContainer>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Gender</TableCell>
-                  <TableCell>Birth Year</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {characters.map((character, index) => (
-                  <Character
-                    key={uuidv4()}
-                    ref={index + 1 === characters.length ? lastCharacterRef : undefined}
-                    name={character.name}
-                    gender={character.gender}
-                    birth_year={character.birth_year}
-                    height={+character.height}
-                    films={character.films}
-                  />)
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
-
-        </div>)}
-      <div>
-        {isLoading && <p>Loading...</p>}
-      </div>
-      <div>
-        {!hasMore && <p>Nothing to load</p>}
-      </div>
-    </div>
+            <TableContainer className={classes.container}>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>
+                      Name
+                  </TableCell>
+                    <TableCell>Gender</TableCell>
+                    <TableCell>Birth Year</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {characters.map((character, index) => (
+                    <Character
+                      key={uuidv4()}
+                      ref={index + 1 === characters.length ? lastCharacterRef : undefined}
+                      name={character.name}
+                      gender={character.gender}
+                      birth_year={character.birth_year}
+                      height={+character.height}
+                      films={character.films}
+                    />)
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
+          <div>
+            {isLoading && <p>Loading...</p>}
+          </div>
+          <div>
+            {!hasMore && <p>Nothing to load</p>}
+          </div>
+        </Paper>
+      </MainWrapper>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styled/MainWrapper.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const MainWrapper = styled.div`
+    display: flex;
+    background: #222;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`
+
+export default MainWrapper
